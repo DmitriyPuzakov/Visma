861a1aa57e6b35e02ceb6905cc44bbac6501af8b
Dmitriy <dp@pqlab.ru>
2019-03-10 17:03:52 +0300

    rename controller

---
+++ Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Visma.Models;
+
+namespace Visma.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bank = new BankAccount("123456-785");
+            bank.GetLongFormat();
+            return View();
+        }
+
+        [HttpPost]
+        public string Process(string shortNumber) {
+            return "ok";
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -67,7 +67,7 @@ namespace Visma
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=App}/{action=Index}/{id?}");
             });
         }
     }
