a1b99d984fa9e0edffaf22b2eabd73670c16f414
Dmitriy <dp@pqlab.ru>
2019-03-10 17:01:47 +0300

    add some scripts

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+$(document).ready(() => {
+    $("#process").click(() => {
+        var shortNumber = $("#shortNumber").val();
+        
+        $.ajax({
+            url:"/App/Process",
+            type:"POST",
+            data: {
+                shortNumber: shortNumber
+            },
+            dataType:"json",
+            success:(resp) => {
+                console.log(resp);
+            }
+        });
+    })
+});
